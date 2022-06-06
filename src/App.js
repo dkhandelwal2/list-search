93aec49006761f28911db4b4ad92c774cb66f2ae
grappus-UI-dev <deepak.khandelwal@grappus.com>
2022-06-06 18:59:50 +0530

    FIX: search logic

--- src/App.js
+++ src/App.js
@@ -3,31 +3,37 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [APIData, setAPIData] = useState([]);
-  const [filteredResults, setFilteredResults] = useState([]);
+  const [allData, setAllData] = useState([]);
+  const [filteredData, setFilteredData] = useState(allData);
   const [searchInput, setSearchInput] = useState("");
+
   useEffect(() => {
     axios
       .get(`https://run.mocky.io/v3/b606e1f7-74f2-429d-93de-32ce62ab7901`)
       .then((response) => {
-        setAPIData(response.data);
-      });
+        setAllData(response.data);
+        setFilteredData(response.data);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
-  const searchItems = (searchValue) => {
-    setSearchInput(searchValue);
-    if (searchInput !== "") {
-      const filteredData = APIData.filter((item) => {
-        return Object.values(item)
-          .join("")
-          .toLowerCase()
-          .includes(searchInput.toLowerCase());
-      });
-      setFilteredResults(filteredData);
-    } else {
-      setFilteredResults(APIData);
-    }
-  };
+  const handleChange = (e) => {
+    let value = e.target.value.toLowerCase();
+    setSearchInput(value);
+
+    let result1 = [];
+    let result2 = [];
+    let result3 = [];
+    let result4 = [];
+
+    result1 = allData.filter((data) => data.first_name.toLowerCase().search(value) !== -1);
+    result2 = allData.filter((data) => data.last_name.toLowerCase().search(value) !== -1);
+    result3 = allData.filter((data) => data.gender.toLowerCase().search(value) !== -1);
+    result4 = allData.filter((data) => data.address.toLowerCase().search(value) !== -1);
+
+    let result = result1.concat(result2, result3, result4);
+    setFilteredData(result);
+  }
 
   return (
     <div className='app'>
@@ -36,11 +42,12 @@ function App() {
         className={`search-field ${searchInput.length > 1 ? "data-shown" : ""}`}
         type="search"
         placeholder="Search"
-        onChange={(e) => searchItems(e.target.value)}
+        // onChange={(e) => searchItems(e.target.value)}
+        onChange={(e) => handleChange(e)}
       />
       <div className={`search-data-box ${searchInput.length > 1 ? "" : "no-data"}`}>
         {searchInput.length > 1
-          ? filteredResults.map((item) => {
+          ? filteredData.map((item) => {
             return (
               <>
                 <div className='user-data'>
